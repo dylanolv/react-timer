98648cf8dd1e665ff964ca3e50b51216b4e2a4ee
dylanolv <oliveira.dylann@gmail.com>
2018-11-30 10:49:23 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,142 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class AddCompteur extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listeDeCompteur: []
+        };
+        this.addComponent = this.addComponent.bind(this);
+    }
+
+    addComponent(event) {
+        const listeDeCompteur = this.state.listeDeCompteur;
+        this.setState({
+            listeDeCompteur: listeDeCompteur.concat(<Timer minutes={10} />)
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <button class="button" onClick={this.addComponent}>
+                    Ajouter un compteur
+                </button>
+                {this.state.listeDeCompteur}
+            </div>
+        );
+    }
 }
+
+class Timer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            libelle: "",
+            minutes: "",
+            secondes: "00",
+            finished: false
+        };
+        this.stopCountDown = this.stopCountDown.bind(this);
+        this.restartCountDown = this.restartCountDown.bind(this);
+    }
+
+    tick() {
+        let sec = this.state.secondes;
+
+        this.setState({
+            secondes: this.state.secondes - 1
+        });
+
+        if (sec === "00") {
+            this.setState({
+                minutes: this.state.minutes - 1,
+                secondes: 59
+            });
+        }
+
+        if (this.state.secondes < 10) {
+            this.setState({
+                secondes: "0" + this.state.secondes
+            });
+        }
+
+        if (this.state.minutes === 0 && this.state.secondes === "00") {
+            this.setState({
+                finished: true
+            });
+            clearInterval(this.timer);
+        }
+    }
+
+    stopCountDown() {
+        clearInterval(this.timer);
+    }
+
+    restartCountDown() {
+        if (this.state.finished === false) {
+            this.setState({
+                secondes: this.state.secondes,
+                minutes: this.state.minutes
+            });
+
+            this.timer = setInterval(() => this.tick(), 1000);
+        } else {
+            return false;
+        }
+    }
+
+    handleChangeLibelle = e => {
+        this.setState({
+            libelle: e.target.value
+        });
+    };
+
+    handleChangeMinutes = e => {
+        this.setState({
+            minutes: e.target.value
+        });
+    };
+
+    render() {
+        return (
+            <div class="compteur">
+                <h2>{this.state.libelle}</h2>
+                <input
+                    type="text"
+                    name="libelle"
+                    onChange={this.handleChangeLibelle}
+                    placeholder="Libelle"
+                />
+                <input
+                    type="number"
+                    name="minutes"
+                    onChange={this.handleChangeMinutes}
+                    placeholder="Minutes"
+                />
+                <h3>
+                    {" "}
+                    {this.state.minutes}:{this.state.secondes}
+                </h3>
+                <button class="button" onClick={this.stopCountDown}>
+                    Pause
+                </button>
+                <button class="button" onClick={this.restartCountDown}>
+                    Start
+                </button>
+            </div>
+        );
+    }
+}
+
+const App = () => {
+    return (
+        <div>
+            <AddCompteur />
+            <Timer />
+            <Timer />
+        </div>
+    );
+};
 
 export default App;
